6b4aa525f5e0bd7518f7ef3e4263b79d97441954
lentychang <lentychang@gmail.com>
2019-12-17 00:32:36 +0100

    add main function to microblog

--- microblog.py
+++ microblog.py
@@ -8,3 +8,7 @@ cli.register_cli(app)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == "__main__":
+    app.run(port='5000')
