1ed6cf5718f518204195897329cc8da549c1c307
lentychang <lentychang@gmail.com>
2020-01-21 18:21:46 +0100

    update notification reqularly

--- app/main/routes.py
+++ app/main/routes.py
@@ -13,6 +13,8 @@ from app.models import Post, User
 from app.main import bp
 from app.main.forms import MessageForm
 from app.models import Message
+from app.models import Notification
+
 
 @bp.before_request
 def before_request():
@@ -187,6 +189,8 @@ def send_message(recipient):
                       body=form.message.data)
         db.session.add(msg)
         db.session.commit()
+        user.add_notification('unread_message_count', user.new_messages())
+        db.session.commit()
         flash(_('Your message has been sent.'))
         return redirect(url_for('main.user', username=recipient))
     return render_template('send_message.html', title=_('Send Message'),
@@ -197,6 +201,7 @@ def send_message(recipient):
 @login_required
 def messages():
     current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
     db.session.commit()
     page = request.args.get('page', 1, type=int)
     messages = current_user.messages_received.order_by(
@@ -208,3 +213,16 @@ def messages():
         if messages.has_prev else None
     return render_template('messages.html', messages=messages.items,
                            next_url=next_url, prev_url=prev_url)
+
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    notifications = current_user.notifications.filter(
+        Notification.timestamp > since).order_by(Notification.timestamp.asc())
+    return jsonify([{
+        'name': n.name,
+        'data': n.get_data(),
+        'timestamp': n.timestamp
+    } for n in notifications])
--- microblog.py
+++ microblog.py
@@ -1,13 +1,15 @@
 from app import create_app, db, cli
 from app.models import User, Post
 from app import cli
+from app.models import User, Post, Notification, Message
 
 app = create_app()
 cli.register_cli(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
+            'Notification': Notification}   
 
 
 if __name__ == "__main__":
