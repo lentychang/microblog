4a9e8bd692fbf081090f6793b737df756026fc8d
lentychang <lentychang@gmail.com>
2020-01-24 08:24:55 +0100

    add auth to api

--- app/models.py
+++ app/models.py
@@ -8,9 +8,11 @@ import jwt
 from app.search import add_to_index, remove_from_index, query_index
 from flask import current_app, url_for
 import json
-from time import time
 import redis
 import rq
+import base64
+from datetime import datetime, timedelta
+import os
 
 
 class SearchableMixin(object):
@@ -114,6 +116,9 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                                     lazy='dynamic')
     tasks = db.relationship('Task', backref='user', lazy='dynamic')
 
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -212,6 +217,25 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                 setattr(self, field, data[field])
         if new_user and 'password' in data:
             self.set_password(data['password'])
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
 
 class Post(SearchableMixin, db.Model):
